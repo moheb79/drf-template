21c2ac2159ac57aaa235d7223d2167ddfd9126b2
mohamadhosseinbahador <mhmdhbhadr@gmail.com>
2024-08-10 13:40:20 +0330

    feat:Add vscode setting and configure the black-formatter

--- drfecommerce/manage.py
+++ drfecommerce/manage.py
@@ -8,11 +8,11 @@ from drfecommerce.settings import base
 def main():
     """Run administrative tasks."""
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'drfecommerce.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfecommerce.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'drfecommerce.settings.production')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "drfecommerce.settings.production"
+        )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -24,5 +24,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
